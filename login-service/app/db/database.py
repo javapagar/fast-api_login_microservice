e8d350ff6be376f8e58a7f0204bde2b74f6dcd49
javapagar <apariciogarcia.j@gmail.com>
2021-07-31 18:58:11 +0200

    Final code

--- login-service/app/db/database.py
+++ login-service/app/db/database.py
@@ -6,11 +6,11 @@ from config import Settings
 
 settings = Settings()
 
-SQLALCHEMY_DATABASE_URL = settings.db_url
-#SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
+#SQLALCHEMY_DATABASE_URL = settings.db_url
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, 
-        connect_args={"check_same_thread": False}
+        #connect_args={"check_same_thread": False}
         )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
